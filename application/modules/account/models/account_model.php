8bf0833ff28f1a98f9da201cc331cb3a48e793d4
Dương Bùi Quý <bui.quy.duong@framgia.com>
2013-02-22 11:50:49 +0700

    Nothing to comit

--- application/modules/account/models/account_model.php
+++ application/modules/account/models/account_model.php
@@ -4,82 +4,76 @@
  *
  * @author duongbq
  */
-class Account_model extends CI_Model
-{
+class Account_model extends CI_Model {
 
-    function __construct()
-    {
-        parent::__construct();
-    }
+	function __construct() {
+		parent::__construct();
+	}
 
-    private function _set_where($params)
-    {
+	private function _set_where($params) {
 
-        if (isset($params['id']))
-            $this->db->where('id', $params['id']);
+		if (isset($params['id']))
+			$this -> db -> where('id', $params['id']);
 
-        if (isset($params['email']))
-            $this->db->where('email', $params['email']);
+		if (isset($params['email']))
+			$this -> db -> where('email', $params['email']);
 
-        if (isset($params['username']))
-            $this->db->where('username', $params['username']);
+		if (isset($params['username']))
+			$this -> db -> where('username', $params['username']);
 
-        if (isset($params['password_digest']))
-            $this->db->where('password_digest', $params['password_digest']);
+		if (isset($params['password_digest']))
+			$this -> db -> where('password_digest', $params['password_digest']);
 
-        if (isset($params['disable_account']))
-            $this->db->where('disable_account', $params['disable_account']);
+		if (isset($params['disable_account']))
+			$this -> db -> where('disable_account', $params['disable_account']);
 
-        if (isset($params['sex']))
-            $this->db->where('sex', $params['sex']);
-    }
+		if (isset($params['sex']))
+			$this -> db -> where('sex', $params['sex']);
+	}
 
-    function get_accounts($params = array())
-    {
+	function get_accounts($params = array()) {
 
-        $this->_set_where($params);
+		$this -> _set_where($params);
 
-        if (isset($params['order_by']))
-            $this->db->order_by($params['order_by']);
-        else
-            $this->db->order_by('first_name');
+		if (isset($params['order_by']))
+			$this -> db -> order_by($params['order_by']);
+		else
+			$this -> db -> order_by('first_name');
 
-        if (isset($params['limit']) && isset($params['offset']))
-            $this->db->limit($params['limit'], $params['offset']);
+		if (isset($params['limit']) && isset($params['offset']))
+			$this -> db -> limit($params['limit'], $params['offset']);
 
-        return $this->db->get('accounts')->result();
-    }
+		return $this -> db -> get('accounts') -> result();
+	}
 
-    function get_roles_by_account_id($account_id)
-    {
-        //Account has many roles
-        //Get role_id array from account_role table
-        $this->db->where('account_id', $account_id);
-        $role_id_from_account_role = $this->db->get('account_role')->result();
+	function get_roles_by_account_id($account_id) {
+		//Account has many roles
+		//Get role_id array from account_role table
+		$this -> db -> where('account_id', $account_id);
+		$role_id_from_account_role = $this -> db -> get('account_role') -> result();
 
-        $role_ids = '';
-        foreach ($role_id_from_account_role as $role) {
+		$role_ids = '';
+		foreach ($role_id_from_account_role as $role) {
 
-            if ($role_ids == '')
-                $role_ids = $role->role_id;
-            else
-                $role_ids .= ', ' . $role->role_id;
-        }
+			if ($role_ids == '')
+				$role_ids = $role -> role_id;
+			else
+				$role_ids .= ', ' . $role -> role_id;
+		}
 
-        if ($role_ids != '') {
-            $this->load->model('role/Role_model');
-            return $this->Role_model->get_role_tags(array('id_in_array' => $role_ids));
-        } else {
-            return NULL;
-        }
-    }
+		if ($role_ids != '') {
+			$this -> load -> model('role/Role_model');
+			return $this -> Role_model -> get_role_tags(array('id_in_array' => $role_ids));
+		} else {
+			return NULL;
+		}
+	}
 
-    function update_account($data = array())
-    {
-        if (isset($data['id'])) {
-            $this->db->where('id', $data['id']);
-            $this->db->update('accounts', $data);
-        }
-    }
+	function update_account($data = array()) {
+		if (isset($data['id'])) {
+			$this -> db -> where('id', $data['id']);
+			$this -> db -> update('accounts', $data);
+		}
+	}
+
 }
-
